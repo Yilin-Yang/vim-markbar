d4593c5a5c726afec54150349fd894871bb2164a
Yilin Yang <yiliny@umich.edu>
2018-10-29 17:16:05 -0400

    Parameterize non-English test suite; test in ES

--- test/run_tests_nvim.sh
+++ test/run_tests_nvim.sh
@@ -5,10 +5,18 @@
 # DETAILS:  Taken, in part, from:
 #               https://github.com/junegunn/vader.vim
 #               https://github.com/neovim/neovim/issues/4842
+# PARAM $1: RUN_FULL    If set to '-f' or '--full', re-run tests in non-English
+#                       locales.
+if [ "$1" == '-f' -o "$1" == '--full' ]; then
+    RUN_FULL=1
+fi
 
 set -p
 export VADER_OUTPUT_FILE=/dev/stderr
 nvim --headless -Nnu .lvimrc -i NONE -c 'Vader! *vader'
 
-# test non-English locale
-nvim --headless -Nnu .lvimrc -i NONE -c 'language de_DE.utf8' -c 'Vader! *vader'
+if [ $RUN_FULL ]; then
+    # test non-English locale
+    nvim --headless -Nnu .lvimrc -i NONE -c 'language de_DE.utf8' -c 'Vader! *vader'
+    nvim --headless -Nnu .lvimrc -i NONE -c 'language es_ES.utf8' -c 'Vader! *vader'
+fi
--- test/run_tests_vim.sh
+++ test/run_tests_vim.sh
@@ -5,9 +5,17 @@
 # DETAILS:  Taken, in part, from:
 #               https://github.com/junegunn/vader.vim
 #               https://github.com/neovim/neovim/issues/4842
+# PARAM $1: RUN_FULL    If set to '-f' or '--full', re-run tests in non-English
+#                       locales.
+if [ "$1" == '-f' -o "$1" == '--full' ]; then
+    RUN_FULL=1
+fi
 
 set -p
 vim -Nnu .lvimrc -i NONE -c 'Vader! *vader'
 
-# test non-English locale
-vim -Nnu .lvimrc -i NONE -c 'language de_DE.utf8' -c 'Vader! *vader'
+if [ $RUN_FULL ]; then
+    # test non-English locale
+    vim -Nnu .lvimrc -i NONE -c 'language de_DE.utf8' -c 'Vader! *vader'
+    vim -Nnu .lvimrc -i NONE -c 'language es_ES.utf8' -c 'Vader! *vader'
+fi
